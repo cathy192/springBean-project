7034202a94d0d5fe7aae8f6a202f6af69398392e
cathy192 <korea@2047>
2020-12-04 19:54:18 +0900

    Appconfig 파일을 역할이 잘 드러나도록 리팩토링

--- core/src/main/java/hello/core/Appconfig.java
+++ core/src/main/java/hello/core/Appconfig.java
@@ -1,19 +1,31 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+
 public class Appconfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        //인터페이스를 반환해주는 역활
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+    public DiscountPolicy discountPolicy(){
+        //인터페이스를 반환해주는 역활
+        return new FixDiscountPolicy();
     }
 }
