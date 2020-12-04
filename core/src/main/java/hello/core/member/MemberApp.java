bb75fa2e26b9ae06bb656de81d2d9eee364d9a26
cathy192 <korea@2047>
2020-12-05 00:20:52 +0900

    bean으로 등록하고 조회하기

--- core/src/main/java/hello/core/member/MemberApp.java
+++ core/src/main/java/hello/core/member/MemberApp.java
@@ -1,12 +1,19 @@
 package hello.core.member;
 
 import hello.core.Appconfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        Appconfig appconfig= new Appconfig();
-        MemberService memberService= appconfig.memberService();
+        //Appconfig appconfig= new Appconfig();
+        //MemberService memberService= appconfig.memberService();
+
+        //얘가 @bean으로 되있는거 다 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Appconfig.class);
+        // 메서드 이름으로 접근. 두번째 인자는 타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 
