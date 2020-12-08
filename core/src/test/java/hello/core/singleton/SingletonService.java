fc70173b55f3ee640295122e75efb13b22eab62b
cathy192 <korea@2047>
2020-12-08 23:34:43 +0900

    싱글톤 패턴과 순수 DI패턴의 차이점과 문제점

---
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+public class SingletonService {
+    //자기 자신을 static으로 가져 하나만 갖게됨
+    private static final SingletonService instance = new SingletonService();
+
+    public static  SingletonService getInstance(){
+        return instance;
+    }
+
+    private SingletonService(){
+    }
+    public  void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+}
